37b6a52140ef027b1e08814e7fd0722f35df3609
Damián Nicolás Alderete <damian.aldert@gmail.com>
2025-04-30 18:57:17 -0300

    puse imagenes

--- componentes/gallery.jsx
+++ componentes/gallery.jsx
@@ -8,6 +8,9 @@ const Gallery = () => {
     "https://prod-cms-static.ticketek.com.ar/sites/default/files/styles/artists_list_featured/public/images/show-header/master_s.png",
     "  https://prod-cms-static.ticketek.com.ar/sites/default/files/styles/artists_list_featured/public/images/show-header/sub_25_s.png",
     "  https://prod-cms-static.ticketek.com.ar/sites/default/files/styles/artists_list_featured/public/images/show-header/galaxias_s.png",
+    "https://prod-cms-static.ticketek.com.ar/sites/default/files/styles/artists_list_featured/public/images/show-header/amer960.png",
+    "https://prod-cms-static.ticketek.com.ar/sites/default/files/images/show-header/padelba960.png",
+    "https://prod-cms-static.ticketek.com.ar/sites/default/files/styles/artists_list_featured/public/principito_g_s.png",
   ];
 
   return (
